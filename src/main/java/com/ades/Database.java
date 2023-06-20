05f30e4735bfadde0d00a8d1c990db2cbc2cebbe
dddingnan <silenceapple1027@gmail.com>
2023-06-20 18:01:50 -0400

    feat: add database select, insert, update.

--- src/main/java/com/ades/Database.java
+++ src/main/java/com/ades/Database.java
@@ -4,21 +4,24 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 
 public class Database {
-    private static final String SQLITE_CONN_STRING = "jdbc:sqlite:YourDatabaseName.db";
+    private static final String SQLITE_CONN_STRING = "jdbc:sqlite:C:/sqlite/database";
+    private static final String DATABASE_NAME = "cs601-dingnan.db";
 
-    public static void createNewDatabase(String fileName) {
-        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+    public static void createNewDatabase() {
+        String url = SQLITE_CONN_STRING + "/" + DATABASE_NAME;
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) {
                 DatabaseMetaData meta = conn.getMetaData();
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
             }
-
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
@@ -26,14 +29,14 @@ public class Database {
         Connection conn = null;
         try {
             // db parameters
-            String url = SQLITE_CONN_STRING;
+            String url = SQLITE_CONN_STRING + "/" + DATABASE_NAME;
             // create a connection to the database
             conn = DriverManager.getConnection(url);
-            
+
             System.out.println("Connection to SQLite has been established.");
-            
+
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
         return conn;
     }
@@ -44,8 +47,72 @@ public class Database {
                 conn.close();
                 System.out.println("Connection to SQLite has been closed.");
             }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void createNewTable() {
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS airplanes (\n"
+                + " id integer PRIMARY KEY,\n"
+                + " name text NOT NULL,\n"
+                + " fuel_capacity integer NOT NULL,\n"
+                + " fuel_consumption integer NOT NULL,\n"
+                + " speed integer NOT NULL\n"
+                + ");";
+        String url = SQLITE_CONN_STRING + "/" + DATABASE_NAME;
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void selectAll(String tableName) {
+        String sql = "SELECT * FROM " + tableName;
+        try (Connection conn = connect();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getInt("fuel_capacity") + "\t" +
+                        rs.getInt("fuel_consumption") + "\t" +
+                        rs.getInt("speed"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void insert(String tableName, String name, int fuel_capacity, int fuel_consumption, int speed) {
+        String sql = "INSERT INTO " + tableName + "(name,fuel_capacity,fuel_consumption,speed) VALUES(?,?,?,?)";
+        try (Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setInt(2, fuel_capacity);
+            pstmt.setInt(3, fuel_consumption);
+            pstmt.setInt(4, speed);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void update(String tableName, String name, int fuel_capacity, int id) {
+        String sql = "UPDATE " + tableName + " SET name = ? , "
+                + "fuel_capacity = ? "
+                + "WHERE id = ?";
+        try (Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setInt(2, fuel_capacity);
+            pstmt.setInt(3, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- src/main/java/com/ades/Main.java
+++ src/main/java/com/ades/Main.java
@@ -9,41 +9,50 @@ import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) throws InvalidDataException, InterruptedException, ExecutionException {
+
         Connection conn = null;
         try {
-            Database.createNewDatabase("test.db");
-            // ... rest of your code
+            // Database.createNewDatabase();
+            conn = Database.connect();
+            // Database.createNewTable();
+            // Database.insert("airplanes", "Boeing 747", 20000, 5, 900);
+            // Database.selectAll("airplanes");
+            // Database.update("airplanes", "Airbus A380", 30000, 1);
+            Database.selectAll("airplanes");
         } finally {
-            // Database.close(conn);
+            Database.close(conn);
         }
 
-        // System.out.println("Hello! Welcome to the Airplane Destination Evaluation System!");
+        // System.out.println("Hello! Welcome to the Airplane Destination Evaluation
+        // System!");
         // System.out.println("--------------------------------------------------------");
         // List<Location> locations = new ArrayList<>();
         // List<Airplane> airplanes = new ArrayList<>();
         // FileLoader loader = new FileLoader();
         // try {
-        //     Season currentSeason = SeasonUtils.getCurrentSeason();
-        //     System.out.println("User current season: " + currentSeason);
-        //     System.out.println("--------------------------------------------------------");
-        //     locations = loader.loadLocationsFromFile("src/data/locations.csv");
-        //     airplanes = loader.loadAirplanesFromFile("src/data/airplanes.csv");
-        //     List<Weather<Season>> weatherList = loader.loadWeatherFromFile("/data/weather.json");
-        //     // Get the user's current data and determine the season
-        //     Weather<Season> currentWeather = weatherList.stream()
-        //             .filter(weather -> weather.getSeason() == currentSeason)
-        //             .findFirst()
-        //             .orElse(null);
-        //     TravelCalculator travelCalculator = new TravelCalculator(locations,
-        //             currentWeather);
-        //     UserInterface ui = new UserInterface(locations, airplanes, travelCalculator);
-        //     ui.start();
+        // Season currentSeason = SeasonUtils.getCurrentSeason();
+        // System.out.println("User current season: " + currentSeason);
+        // System.out.println("--------------------------------------------------------");
+        // locations = loader.loadLocationsFromFile("src/data/locations.csv");
+        // airplanes = loader.loadAirplanesFromFile("src/data/airplanes.csv");
+        // List<Weather<Season>> weatherList =
+        // loader.loadWeatherFromFile("/data/weather.json");
+        // // Get the user's current data and determine the season
+        // Weather<Season> currentWeather = weatherList.stream()
+        // .filter(weather -> weather.getSeason() == currentSeason)
+        // .findFirst()
+        // .orElse(null);
+        // TravelCalculator travelCalculator = new TravelCalculator(locations,
+        // currentWeather);
+        // UserInterface ui = new UserInterface(locations, airplanes, travelCalculator);
+        // ui.start();
         // } catch (FileNotFoundException e) {
-        //     System.out.println("File not found. Please check the file name and try again.");
-        //     e.printStackTrace();
+        // System.out.println("File not found. Please check the file name and try
+        // again.");
+        // e.printStackTrace();
         // } catch (IOException e) {
-        //     System.out.println("Error occurred while reading the file.");
-        //     e.printStackTrace();
+        // System.out.println("Error occurred while reading the file.");
+        // e.printStackTrace();
         // }
     }
 }
